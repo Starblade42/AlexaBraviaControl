2a7d7aebe37744c105abc385a498504ffe17f024
Jonathan Huff <zarcos@gmail.com>
2016-11-05 18:55:29 -0700

    start small with working example

--- AlexaBraviaControl.py
+++ AlexaBraviaControl.py
@@ -24,7 +24,7 @@ class device_handler(debounce_handler):
     """Publishes the on/off state requested,
        and the IP address of the Echo making the request.
     """
-    TRIGGERS = {"HDMI1": 52000, "HDMI2": 52001, "HDMI2": 52002, "HDMI4": 52003}
+    TRIGGERS = {"device": 52000}
 
     def act(self, client_address, state):
         print "State", state, "from client @", client_address
